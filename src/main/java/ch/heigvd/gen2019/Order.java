6e9ccc0b203041df5de79dc7c51e7ae6b290ac57
HDubuis <helene.dubuis@heig-vd.ch>
2020-05-24 23:25:01 +0200

    Refactor :Part of the code of Orders.getContents corresponded better to Order.toString so I created it

--- src/main/java/ch/heigvd/gen2019/Order.java
+++ src/main/java/ch/heigvd/gen2019/Order.java
@@ -26,4 +26,25 @@ public class Order {
     public void AddProduct(Product product) {
         products.add(product);
     }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("{");
+        sb.append("\"id\": ");
+        sb.append(getOrderId());
+        sb.append(", ");
+        sb.append("\"products\": [");
+        for (int i = 0; i < getProductsCount(); i++) {
+            sb.append(getProduct(i).toString());
+        }
+
+        if (getProductsCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        sb.append("]");
+        sb.append("}, ");
+        return sb.toString();
+    }
 }
